381972e75fdd31da565880351781c4de626a2a46
Saurabh <saurabhv560@gmail.com>
2024-02-22 14:12:42 +0530

    First React Native project

---
+++ AppPro.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+import {
+    View,
+    SafeAreaView,
+    Text,
+    StyleSheet,
+    useColorScheme
+} from'react-native'
+
+function AppPro(): JSX.Element{
+    const isdarkmode= useColorScheme() === 'dark'
+    return(
+       <View style={styles.container}>
+        <Text style={isdarkmode?styles.whiteText : styles.darkText}>hello World@!</Text>
+
+       </View> 
+    )
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        alignItems:'center'
+    },
+    whiteText:{
+        color: '#FFFFFF'
+    },
+    darkText:{
+        color:'#000000'
+    }
+})
+
+
+export default AppPro;
